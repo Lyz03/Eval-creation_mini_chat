df7ebbab25d065dd92df578a8abb61c14c010a0c
Lyz03 <lizoe@netcourrier.com>
2022-04-01 16:11:08 +0200

    session start

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!doctype html>
 <html lang="fr">
 <head>
@@ -12,10 +15,7 @@
 
 <?php
 
-session_start();
-
 $param = $_GET['p'] ?? null;
-
 
 switch ($param) {
     case 'chat':
